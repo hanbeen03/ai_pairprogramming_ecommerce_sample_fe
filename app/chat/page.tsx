532c71205f730450ed16204f3fc3cdf6ff87feaf
hanbeen03 <hanbeen03@gmail.com>
2025-02-26 12:55:18 +0900

    add chatbot

---
+++ app/chat/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { CsChat } from '@/components/component/cs-chat';
+
+const Page = () => {
+    return (
+        <div>
+            <CsChat />
+        </div>
+    );
+}
+
+export default Page;
--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: [
-      'next/babel', // Next.js 프로젝트일 경우
-      '@babel/preset-env',
-      '@babel/preset-react',
-      '@babel/preset-typescript'
-    ]
-  };
-  
